1e8a4aa12a1d8a567d0837639bc26ee5ed250b4a
Leonard <duy26332006@gmail.com>
2026-02-09 22:21:58 +0700

    delete duplicate folder

--- Popup-Translation-FE/src/Data/data.ts
+++
@@ -1,5 +0,0 @@
-export const NavbarMenu = [
-    { id: 1, title: 'Home', link: '/' },
-    { id: 2, title: 'Vocabulary', link: '/vocabulary' },
-    { id: 3, title: 'Practice', link: '/practice' },
-]
